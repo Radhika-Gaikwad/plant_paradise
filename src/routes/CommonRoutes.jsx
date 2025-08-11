508c6887a8fd0c8fa55588889b3eaa4173211ae3
Gaikwad Radhika <radhika.sai.gaikwad@gmail.com>
2025-08-11 19:19:01 +0530

    fixed login button

--- src/routes/CommonRoutes.jsx
+++ src/routes/CommonRoutes.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 
-import Login from "../features/authentication/LoginForm";
+import LoginForm from "../features/authentication/LoginForm";
 import SignUpForm from "../features/authentication/SignUpForm";
 import Address from "../features/address/Address";
 
 const CommonRoutes = () => {
   return (
     <Routes>
-      <Route path="/login" element={<Login />} />
+      <Route path="/login" element={<LoginForm />} />
       <Route path="/signup" element={<SignUpForm />} />
       <Route path="/add-address" element={<Address />} />
     </Routes>
